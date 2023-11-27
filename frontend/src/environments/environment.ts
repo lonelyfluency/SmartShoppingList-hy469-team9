9b8534b077d9728f793a013eea3b1cc48d7d01c0
Dada Tian <lonelyfluency@gmail.com>
2023-11-27 15:57:05 +0200

    Change host ip to suit multi devices

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  host: 'http://localhost:8080', // change "localhost" to your IP address
+  // host: 'http://localhost:8080', // change "localhost" to your IP address
+  host: 'http://192.168.137.1:8080',
 };
 /*
  * For easier debugging in development mode, you can import the following file
