32b816d56c98df145afbb0c8a734bffa43310033
Dada Tian <lonelyfluency@gmail.com>
2023-12-05 15:32:45 +0200

    Shop login bug fixxed
    Follow angular design, change the javascripts in html file to typescripts in ts file.

--- frontend/src/app/pages/shop_login/shop_login.component.ts
+++ frontend/src/app/pages/shop_login/shop_login.component.ts
@@ -1,31 +1,54 @@
-import { Component } from '@angular/core';
-import { WebcamImage } from 'ngx-webcam';
-import { Subject, Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-shop_login',
+  selector: 'app-shop-login',
   templateUrl: './shop_login.component.html',
   styleUrls: ['./shop_login.component.scss']
 })
+export class ShopLoginComponent implements OnInit, OnDestroy {
+  @ViewChild('video') videoElement!: ElementRef;
+  @ViewChild('canvas') canvasElement!: ElementRef;
+  stream: MediaStream | null = null;
 
-export class ShopLoginComponent {
+  ngOnInit() {
+    // This lifecycle hook gets called once the component's view has been fully initialized
+  }
 
+  ngOnDestroy() {
+    this.stopMedia();
+  }
+
+  getMedia() {
+    const constraints = {
+      video: { width: 500, height: 500 },
+      audio: true
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(stream => {
+        this.stream = stream;
+        if (this.videoElement && this.videoElement.nativeElement) {
+          this.videoElement.nativeElement.srcObject = stream;
+          this.videoElement.nativeElement.play();
+        }
+      })
+      .catch(error => console.error(error));
+  }
+
+  takePhoto() {
+    if (this.canvasElement && this.canvasElement.nativeElement && this.videoElement && this.videoElement.nativeElement) {
+      const ctx = this.canvasElement.nativeElement.getContext('2d');
+      ctx.drawImage(this.videoElement.nativeElement, 0, 0, 300, 300);
+    }
+  }
+
+  stopMedia() {
+    if (this.stream) {
+      const tracks = this.stream.getTracks();
+      tracks.forEach(track => track.stop());
+    }
+    if (this.videoElement && this.videoElement.nativeElement) {
+      this.videoElement.nativeElement.srcObject = null;
+    }
+  }
 }
-
-// export class ShopLoginComponent {
-//   title = 'Camera Realtime';
-  
-//   public webcamImage: WebcamImage = null;
-//   private trigger: Subject<void> = new Subject<void>();
-//   triggerSnapshot(): void {
-//     this.trigger.next();
-//   }
-//   handleImage(webcamImage: WebcamImage): void {
-//     console.info('Saved webcam image', webcamImage);
-//     this.webcamImage = webcamImage;
-//   }
-  
-//   public get triggerObservable(): Observable<void> {
-//     return this.trigger.asObservable();
-//   }
-// }
