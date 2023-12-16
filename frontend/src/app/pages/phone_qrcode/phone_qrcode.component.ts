418f00990186836a7df90ee98ebc5e4c212a5189
Iraklis Palaiologos <iraklispal7@gmail.com>
2023-12-16 21:57:40 +0200

    Added Phone Qr code. There is a problem with camera in the qr code and the log in page .

--- frontend/src/app/pages/phone_qrcode/phone_qrcode.component.ts
+++ frontend/src/app/pages/phone_qrcode/phone_qrcode.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-phone_qrcode',
   templateUrl: './phone_qrcode.component.html',
   styleUrls: ['./phone_qrcode.component.scss']
 })
-export class PhoneQrCodeComponent {
+export class PhoneQrCodeComponent   implements OnInit, OnDestroy{
+
+  @ViewChild('video') videoElement!: ElementRef;
+  @ViewChild('canvas') canvasElement!: ElementRef;
+  stream: MediaStream | null = null;
+
+  ngOnInit() {
+    // This lifecycle hook gets called once the component's view has been fully initialized
+  }
+
+  ngOnDestroy() {
+    this.stopMedia();
+  }
+
+  getMedia() {
+    const constraints = {
+      video: { width: 500, height: 500 },
+      audio: true
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(stream => {
+        this.stream = stream;
+        if (this.videoElement && this.videoElement.nativeElement) {
+          this.videoElement.nativeElement.srcObject = stream;
+          this.videoElement.nativeElement.play();
+        }
+      })
+      .catch(error => console.error(error));
+  }
+
+  takePhoto() {
+    if (this.canvasElement && this.canvasElement.nativeElement && this.videoElement && this.videoElement.nativeElement) {
+      const ctx = this.canvasElement.nativeElement.getContext('2d');
+      ctx.drawImage(this.videoElement.nativeElement, 0, 0, 300, 300);
+    }
+  }
+
+  stopMedia() {
+    if (this.stream) {
+      const tracks = this.stream.getTracks();
+      tracks.forEach(track => track.stop());
+    }
+    if (this.videoElement && this.videoElement.nativeElement) {
+      this.videoElement.nativeElement.srcObject = null;
+    }
+  }
 
 }
