edeafb403480eda66fde31f975b12a1637864f8f
lonelyfluency <lonelyfluency@gmail.com>
2024-01-10 17:11:30 +0200

    finished voice command control function, make search function better

--- frontend/src/app/pages/components/nav-bar/nav-bar-routing.module.ts
+++ frontend/src/app/pages/components/nav-bar/nav-bar-routing.module.ts
@@ -5,6 +5,7 @@ import { ShopProductsComponent } from '../../shop_products/shop_products.compone
 import { ShopCartComponent } from '../../shop_cart/shop_cart.component';
 import { ShopOfferComponent } from '../../shop_offer/shop_offer.component';
 import { ShopComponent } from '../../shop/shop.component';
+import { ShopSearchComponent } from '../../shop_search/shop_search.component';
 
 
 
@@ -17,6 +18,7 @@ const routes: Routes = [
         { path: 'shop_products', component: ShopProductsComponent},
         { path: 'shop_cart', component: ShopCartComponent},
         { path: 'shop_offer', component: ShopOfferComponent},
+        { path: 'shop_search', component: ShopSearchComponent},
         { path: '**', redirectTo: 'shop_main', pathMatch: 'full' },]
     },
   
--- frontend/src/app/pages/components/top-bar/top-bar.component.ts
+++ frontend/src/app/pages/components/top-bar/top-bar.component.ts
@@ -1,45 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { SmartSpeakerService } from 'src/app/global/services/smart-speaker/smart-speaker.service';
-
-
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss']
 })
-export class TopBarComponent {
+export class TopBarComponent implements OnInit, OnDestroy {
   searchQuery = '';
-  voiceQuery = '';
   private isRecording = false;
+  private commandSubscription?: Subscription;
 
-  constructor(private router: Router,private smartSpeakerService: SmartSpeakerService) {}
+  constructor(private router: Router, private smartSpeakerService: SmartSpeakerService) {}
 
   ngOnInit() {
-    this.smartSpeakerService.initialize();
-    this.smartSpeakerService.addCommand("search for *", (spokenText: string) => {
-      this.voiceQuery = spokenText.split('search for ')[1];
-    });
+    this.commandSubscription = this.smartSpeakerService.commands$.subscribe(
+      command => {
+        if (command.toLowerCase().startsWith('search for ')) {
+          this.searchQuery = command.split('search for ')[1];
+          this.router.navigate(['/shop/shop_search'], { queryParams: { query: this.searchQuery } });
+        }
+        if (command.toLowerCase().startsWith('go home')) {
+          this.router.navigate(['/shop/shop_main']);
+        }
+        if (command.toLowerCase().startsWith('go to products')) {
+          this.router.navigate(['/shop/shop_products']);
+        }
+        if (command.toLowerCase().startsWith('go to cart')) {
+          this.router.navigate(['/shop/shop_cart']);
+        }
+        if (command.toLowerCase().startsWith('go to offer')) {
+          this.router.navigate(['/shop/shop_offer']);
+        }
+        if (command.toLowerCase().startsWith('go back')) {
+          window.history.back();
+        }
+        if (command.toLowerCase().startsWith('go forward')) {
+          window.history.forward();
+        }
+        if (command.toLowerCase().startsWith('refresh page')) {
+          window.location.reload();
+        }
+
+      }
+    );
   }
 
   onVoiceButtonClick() {
     if (this.isRecording) {
       this.smartSpeakerService.stop();
       this.isRecording = false;
-      this.router.navigate(['/search_result'], { queryParams: { query: this.voiceQuery } });
     } else {
       this.smartSpeakerService.start();
       this.isRecording = true;
-      this.smartSpeakerService.addCommand("search for *", (spokenText: string) => {
-        this.voiceQuery = spokenText.split('search for ')[1];
-      });
     }
   }
 
   onSearch(): void {
-    this.router.navigate(['/search_result'], { queryParams: { query: this.searchQuery } });
+    this.router.navigate(['/shop/shop_search'], { queryParams: { query: this.searchQuery } });
+  }
+
+  ngOnDestroy() {
+    if (this.commandSubscription) {
+      this.commandSubscription.unsubscribe();
+    }
   }
 }
