f1287c89288c35dc693530af4d63aae3318d33f8
lonelyfluency <lonelyfluency@gmail.com>
2024-01-17 12:43:44 +0200

    add home navigation

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() { }
+
+  goToShopMain(): void {
+    this.router.navigate(['/shop/shop_main']);
+  }
+
+  goToFridgeMain(): void {
+    this.router.navigate(['/fridge_main']);
+  }
+
+  goToPhoneApp(): void {
+    this.router.navigate(['/phone_account']);
+  }
 
 }
